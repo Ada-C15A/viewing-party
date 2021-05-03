2b7472617ea4235e275519df81d974dfffaeff5b
janicelichtman <jalichtman@gmail.com>
2021-05-03 16:50:47 -0700

    wave_02_tests_pass

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,9 @@
-#list of disctionaries, where the inner dictionaries have keys: title, genre, and rating 
+#list of disctionaries, where the inner dictionaries represent movies,
+#and have keys: title, genre, and rating 
 movies =[]
 
 def create_movie(title, genre, rating):
-    if title == None or genre == None or rating == None:
+    if not title or not genre or not rating:
         return None
     
     movies.append({"title": title, "genre": genre, "rating": rating})    
@@ -24,3 +25,21 @@ def watch_movie(user_data, title):
     user_data["watched"].append(movie)
     user_data["watchlist"] = [movie for movie in user_data["watchlist"] if not (movie['title'] == title)]
     return user_data 
+
+def get_watched_avg_rating(user_data):
+    if len(user_data["watched"]) == 0:
+        return 0
+    ratings = [movie['rating'] for movie in user_data["watched"]]
+    return sum(ratings) / len(user_data["watched"])
+
+def get_most_watched_genre(user_data):
+    genres = {}
+    if len(user_data["watched"]) == 0:
+        return None
+    for movie in user_data["watched"]:
+        if movie['genre'] in genres.keys():
+            genres[movie['genre']] += 1
+        else:
+            genres[movie['genre']] = 1
+        winners = [genre for genre, count in genres.items() if count == max(genres.values())]   
+    return winners[0]
