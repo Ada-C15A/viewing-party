4fc714bc765d612d288088e23e96cab9f3726c0d
Christina <206peacegypsy@gmail.com>
2021-05-03 22:58:05 -0700

    wave02 three tests passing

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -19,10 +19,26 @@ def watch_movie(user_data, title):
         if title in movie['title']:
             user_data['watchlist'].remove(movie)
             add_to_watched(user_data, movie)
-            print("HERE")
             return user_data
     return user_data
     
+def get_watched_avg_rating(user_data):
+    if len(user_data['watched']) < 1:
+        return 0.0
+    movie_rating_total = 0
+    for movie in user_data['watched']:
+        movie_rating_total += movie['rating']
+    return movie_rating_total / len(user_data['watched'])    
 
-
-
+def get_most_watched_genre(user_data):
+    movie_genre_dictionary = {}
+    for movie in user_data['watched']:
+        if movie['genre'] not in movie_genre_dictionary:
+            movie['genre'] = 1
+            print("HERE: " + str(movie_genre_dictionary))
+            return movie_genre_dictionary 
+        else:    
+            movie['genre'] += 1
+            print("NOW HERE: " + str(movie_genre_dictionary))
+        return movie_genre_dictionary 
+    
