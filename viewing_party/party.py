714d3c7e29f138932bda0be9b5154587f9f38129
Christina <206peacegypsy@gmail.com>
2021-05-03 20:18:39 -0700

    all wave01 tests passing

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -9,3 +9,20 @@ def create_movie(title, genre, rating):
 def add_to_watched(user_data, movie):
     user_data['watched'].append(movie)
     return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data['watchlist'].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data['watchlist']:
+        if title in movie['title']:
+            user_data['watchlist'].remove(movie)
+            add_to_watched(user_data, movie)
+            print("HERE")
+            return user_data
+    return user_data
+    
+
+
+
