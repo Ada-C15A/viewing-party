a222dfb99f1e3b05224cd336f46fd4188d92aba5
janicelichtman <jalichtman@gmail.com>
2021-05-05 11:59:02 -0700

    general code improvements

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -4,25 +4,21 @@ movies =[]
 def create_movie(title, genre, rating):
     if not title or not genre or not rating:
         return None
-    
-    movies.append({"title": title, "genre": genre, "rating": rating})    
-    return movies.pop()
+    return {"title": title, "genre": genre, "rating": rating}
 
 def add_to_watched(user_data, movie):
-    user_data["watched"].append({"title": movie["title"], "genre": movie["genre"], "rating": movie["rating"]})
+    user_data["watched"].append(movie)
     return user_data 
 
 def add_to_watchlist(user_data, movie):
-    user_data["watchlist"].append({"title": movie["title"], "genre": movie["genre"], "rating": movie["rating"]})
+    user_data["watchlist"].append(movie)
     return user_data
 
 def watch_movie(user_data, title):
-    movie = next((movie for movie in user_data["watchlist"] if movie['title'] == title), None) 
-    if movie ==    None:
-        return user_data 
-    
-    user_data["watched"].append(movie)
-    user_data["watchlist"] = [movie for movie in user_data["watchlist"] if not (movie['title'] == title)]
+    for movie in user_data['watchlist']:
+        if movie['title'] == title:   
+            user_data["watched"].append(movie)
+            user_data["watchlist"] = [movie for movie in user_data["watchlist"] if not (movie['title'] == title)]
     return user_data 
 
 def get_watched_avg_rating(user_data):
