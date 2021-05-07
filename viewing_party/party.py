ddc770a28144ab981f5f22224ebd2bcaca3adc11
denisseai <msdenisseanaya@gmail.com>
2021-05-06 17:12:43 -0700

    wave 1 step 4 - tests passing

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,3 +18,11 @@ def add_to_watched(user_data, movie):
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"] = [movie]
     return user_data
+
+def watch_movie(user_data, title):
+    for movie_in_question in user_data["watchlist"]:
+        if movie_in_question["title"] == title:
+            user_data["watchlist"].remove(movie_in_question)
+            user_data["watched"].append(movie_in_question)
+    return user_data
+
