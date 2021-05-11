f8ebe88ba41952673576a0e222fbf0171fbf9a79
Naomi Quinones <naomiquinones@gmail.com>
2021-05-11 01:57:48 -0700

    Gets watch_movie() to pass wave 1 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -6,8 +6,13 @@ def add_to_watched(user_data, movie):
     return user_data
 
 
-def add_to_watchlist():
-    print('hello')
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
 
-def watch_movie():
-    print('hello')
+def watch_movie(user_data,title):
+    for item in user_data["watchlist"]:
+        if item["title"] == title:
+            movie = user_data["watchlist"].pop(user_data["watchlist"].index(item))
+            add_to_watched(user_data,movie)
+    return user_data
