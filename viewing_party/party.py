81a1772c1f2b18a2b222e77ed453fdcb137d4e2c
Naomi Quinones <naomiquinones@gmail.com>
2021-05-11 01:33:58 -0700

    Adds add_to_watched(),add_to_watchlist(),watch_movie()

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,2 +1,13 @@
 def create_movie(movie_title, genre, rating):
    return {"title":movie_title, "genre":genre, "rating":rating} if (movie_title and genre and rating) else None
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist():
+    print('hello')
+
+def watch_movie():
+    print('hello')
