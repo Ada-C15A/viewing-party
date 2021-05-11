abca10397f5318c2b57f079bc5342cc382e20a5f
Naomi Quinones <naomiquinones@gmail.com>
2021-05-11 09:08:50 -0700

    Adds get_watched_avg_rating()

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -16,3 +16,11 @@ def watch_movie(user_data,title):
         if item["title"] == title:
             add_to_watched(user_data,wl.pop(wl.index(item)))
     return user_data
+
+def get_watched_avg_rating(user_data):
+    cumulative_ratings = 0
+    watched = user_data["watched"]
+    for item in watched:
+        cumulative_ratings += item["rating"]
+
+    return cumulative_ratings/len(watched) if len(watched) > 0 else 0
